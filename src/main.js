56b5185b160a54dbe98e1bb1c5ac35a06f43554e
Sam Alexander <samalexander@users.noreply.github.com>
2020-02-06 09:39:19 +0000

    Add support for displaying transactions

--- src/main.js
+++ src/main.js
@@ -3,6 +3,8 @@ import boxen from 'boxen';
 import inquirer from 'inquirer';
 import request from 'request-promise-native';
 import accounting from 'accounting';
+import columnify from 'columnify';
+import chalk from 'chalk';
 
 const currencyMap = {
     GBP: '£',
@@ -10,9 +12,32 @@ const currencyMap = {
     EUR: '€'
 };
 
+const directionMap = {
+    IN: chalk.green,
+    OUT: chalk.red
+};
+
+function isValidDate(dateString) {
+    const regEx = /^\d{4}-\d{2}-\d{2}$/;
+    return dateString.match(regEx) != null;
+}
+
+function getFirstDayOfMonth() {
+    const date = new Date();
+    return new Date(date.getFullYear(), date.getMonth(), 1);
+}
+
+function dateToYMD(date) {
+    var d = date.getDate();
+    var m = date.getMonth() + 1; // Month from 0 to 11
+    var y = date.getFullYear();
+    return '' + y + '-' + (m <= 9 ? '0' + m : m) + '-' + (d <= 9 ? '0' + d : d);
+}
+
 export async function init(config) {
     const questions = [
         {
+            type: 'password',
             name: 'token',
             message: 'Personal Access Token'
         }
@@ -28,8 +53,8 @@ export async function init(config) {
         });
         config.set({ token, accounts });
         spinner.succeed('Accounts saved');
-    } catch (error) {
-        spinner.fail(error.error.error_description);
+    } catch ({ error }) {
+        spinner.fail(error.error_description);
     }
 }
 
@@ -46,12 +71,62 @@ export async function checkBalance(config) {
                     bearer
                 }
             });
-            const balance = accounting.formatMoney(effectiveBalance.minorUnits / 100, { symbol: currencyMap[acc.currency], });
+            const balance = accounting.formatMoney(effectiveBalance.minorUnits / 100, { symbol: currencyMap[acc.currency] });
             balances.push(balance);
         }
         spinner.stop();
         console.log(boxen(balances.join('\n'), { padding: 1, margin: 1, borderStyle: 'double', borderColor: 'green' }));
-    } catch (error) {
-        spinner.fail(error.error.error_description);
+    } catch ({ error }) {
+        spinner.fail(error.error_description);
     }
+}
+
+export async function listTransactions(config) {
+    const accounts = config.get('accounts');
+    const questions = [
+        {
+            type: 'list',
+            name: 'account',
+            message: 'Select account',
+            choices: accounts.map(a => ({ name: a.currency, value: a }))
+        },
+        {
+            name: 'startDate',
+            message: 'Transactions start date (YYYY-MM-DD)',
+            default: dateToYMD(getFirstDayOfMonth()),
+            validate: (input) => {
+                return isValidDate(input) ? true : 'Date is invalid. Re-enter'
+            }
+        }
+    ];
+    const { account, startDate } = await inquirer.prompt(questions);
+    const spinner = ora({ text: 'Fetching transactions...', color: 'yellow' }).start();
+    try {
+        const bearer = config.get('token');
+        const { feedItems } = await request.get(`https://api.starlingbank.com/api/v2/feed/account/${account.accountUid}/category/${account.defaultCategory}`, {
+            json: true,
+            auth: {
+                bearer
+            },
+            qs: {
+                changesSince: new Date(startDate)
+            }
+        });
+        spinner.stop();
+        displayTransactions(feedItems);
+    } catch ({ error }) {
+        spinner.fail(error.error_description);
+    }
+}
+
+function displayTransactions(feedItems) {
+    const columns = columnify(feedItems.map(fi => {
+        return {
+            time: dateToYMD(new Date(fi.transactionTime)),
+            amount: directionMap[fi.direction](accounting.formatMoney(fi.amount.minorUnits / 100, { symbol: currencyMap[fi.amount.currency] })),
+            name: fi.counterPartyName,
+            type: fi.source
+        };
+    }));
+    console.log(columns);
 }
