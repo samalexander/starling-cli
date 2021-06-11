133fdb25af34080c084560a3102dbd2b7f5f2851
Sam Alexander <samalexander@users.noreply.github.com>
2021-06-11 11:58:12 +0100

    feat: make pure esm package
    BREAKING CHANGE: only supports node >= 12

---
+++ bin/starling.js
@@ -0,0 +1,56 @@
+#!/usr/bin/env node
+
+import program from "commander";
+import Configstore from "configstore";
+import updateNotifier from "update-notifier";
+import { readFile } from "fs/promises";
+import {
+  init,
+  checkBalance,
+  listTransactions,
+  listMandates,
+} from "../src/main.js";
+
+const pkg = JSON.parse(
+  await readFile(new URL("../package.json", import.meta.url))
+);
+
+updateNotifier({ pkg }).notify();
+
+const config = new Configstore(pkg.name);
+
+program.version(pkg.version);
+
+program
+  .command("init")
+  .alias("i")
+  .description("Initialise connection to Starling")
+  .action(() => {
+    init(config);
+  });
+
+program
+  .command("balance")
+  .alias("b")
+  .description("Fetch your Starling account balance")
+  .action(() => {
+    checkBalance(config);
+  });
+
+program
+  .command("transactions")
+  .alias("tx")
+  .description("Fetch your Starling account transactions")
+  .action(() => {
+    listTransactions(config);
+  });
+
+program
+  .command("mandates")
+  .alias("dd")
+  .description("Fetch the Direct Debit mandates on your Starling account")
+  .action(() => {
+    listMandates(config);
+  });
+
+program.parse(process.argv);
